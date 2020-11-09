02e716351fbc151b749a46d359f90e93efcce592
Arm-And-Hammer <73447422+Arm-And-Hammer@users.noreply.github.com>
2020-11-09 18:59:17 -0500

    full commit for 11/9/20

--- js/main.js
+++ js/main.js
@@ -1,5 +1,45 @@
 /*----- constants -----*/
+// var player1 = {
+//     color: '#009933';
+//     number: 1;
+// };
+var player1 = 1;
+var player1Color = '#009933';
+// var player2{
+//     color: '#cc00cc';
+//     number: -1;
+// };
+var player2 = -1;
+var player2Color = '#cc00cc';
+// var noPlayer{
+//     color: 'clear';
+//     number: 0;
+// };
+var empty = 0 
+
+
+
 /*----- app's state (variables) -----*/
+let board;  // can use 1 and -1 to represent the players, 0 to represent an empty cell
+// other state variables here
+
+
 /*----- cached element references -----*/
+
+
 /*----- event listeners -----*/
+
+
 /*----- functions -----*/
+function init() {
+    board = [
+      [0, 0 ,0 ,0 ,0 0],  // col 0
+      [0, 0 ,0 ,0 ,0 0],  // col 1
+      [0, 0 ,0 ,0 ,0 0],  // col 2
+      [0, 0 ,0 ,0 ,0 0],  // col 3
+      [0, 0 ,0 ,0 ,0 0],  // col 4
+      [0, 0 ,0 ,0 ,0 0],  // col 5
+      [0, 0 ,0 ,0 ,0 0],  // col 6
+    ];
+    // initialize other state variables below
+  }
